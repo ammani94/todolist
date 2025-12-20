c4867ff254cf2d775761cd8ad8b56cf557b9e8db
Idir Ammani <idirammani@MacBook-Pro-de-Idir-2.local>
2025-12-21 00:25:34 +0100

    conf

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,5 +12,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    host: '0.0.0.0',  // Écoute sur toutes les interfaces réseau
+    port: 5173,       // Port par défaut de Vite
+    strictPort: true, // Échoue si le port est déjà utilisé
   },
 })
